6bb6770fe7208e65274ffcb855a3c3663fa13033
andresantnunes <98063867+andresantnunes@users.noreply.github.com>
2022-07-13 20:22:44 -0300

    Add files via upload

--- operacoes/src/OperacoesLogicas.java
+++ operacoes/src/OperacoesLogicas.java
@@ -5,18 +5,41 @@ public class OperacoesLogicas {
 
         boolean resultado = true && true; // true
         if(resultado){
-            System.out.println("Executa");
+            System.out.println("Executa AND");
         }
 
         resultado = true && false; // false
         if(resultado)
             System.out.println("Não Executa");
-        else
-            System.out.println("Executa");
 
 
-        if(true && false)
+        if(false && false)
             System.out.println("Não Executa");
 
+        resultado = true || true;
+        if (resultado){
+            System.out.println("Executa OR");
+        }
+
+        resultado = true || false;
+        if (resultado){
+            System.out.println("Executa OR");
+        }
+
+        resultado = false || false;
+        if (resultado){
+            System.out.println("Não Executa OR");
+        }
+
+        int x = 10;
+        if (10 <= x || x < -10){ //ou maior ou igual a 10, ou menor do que -10
+            System.out.println("ou maior ou igual a 10, ou menor do que -10 ");
+        }
+//   or     x                           x
+//        ---------(-10) -------- (10) ---------
+//   and                     x
+        if (10 >= x && x > -10){ //intervalo entre -9 e 10
+            System.out.println("intervalo entre -9 e 10");
+        }
     }
 }
