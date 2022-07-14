7b6d4328afbe304ebcf0587cd2c08521e32b1587
andresantnunes <98063867+andresantnunes@users.noreply.github.com>
2022-07-14 19:33:58 -0300

    Add files via upload

---
+++ operacoes/src/Exercicio22.java
@@ -0,0 +1,50 @@
+package src;
+
+import java.util.Scanner;
+
+public class Exercicio22 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o numero1: ");
+        double numero1 = scanner.nextDouble();
+
+        System.out.print("Digite o numero2: ");
+        double numero2 = scanner.nextDouble();
+
+        System.out.print("Digite a operação (1-soma; 2-subtracao; 3-multiplicacao; 4-divisao): ");
+        double opercao = scanner.nextDouble();
+
+        double resultado = 0;
+
+        if(opercao==1){
+            resultado = numero1 + numero2;
+        }else if(opercao==2){
+            resultado = numero1 - numero2;
+        }else if(opercao==3){
+            resultado = numero1 * numero2;
+        }else if(opercao==4){
+            resultado = numero1 / numero2;
+        }else{
+            System.out.println("Operação inválida");
+            return;
+        }
+
+        boolean positivo = resultado>=0 ? true : false;
+
+        boolean par = false;
+        if(resultado % 2 == 0){
+            par = true;
+        }
+        // true       false
+        if(positivo && par){ //false
+            System.out.println("O número "+resultado+" é par e positivo");
+        } else if(par){ // false
+            System.out.println("O número "+resultado+" é par e negativo");
+        } else if (positivo) { // true
+            System.out.println("O número "+resultado+" é impar e positivo");
+        } else {
+            System.out.println("O número "+resultado+" é impar e negativo");
+        }
+    }
+}
---
+++ operacoes/src/TernarioApp.java
@@ -0,0 +1,8 @@
+package src;
+
+public class TernarioApp {
+    public static void main(String[] args) {
+        String resultado = true ? "verdadeiro":"falso";
+        System.out.println(resultado);
+    }
+}
